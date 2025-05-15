ec35b076a82824f918b738ec2f785a5db9c344df
ajitmathpati <ajitmathpati211@gmail.com>
2025-05-15 12:48:30 +0530

    add this

---
+++ virtual_dom/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [frame, setvalue] = useState(" ")
+
+  function getdate(e) {
+
+    e.preventDefault()
+    alert('in function')
+    const value = e.target.nm.value
+    setvalue(
+      value
+    ); 
+    console.log(value);
+    
+
+  }
+  return (
+    <>
+      <h1>welcome!!!</h1>
+      <h2>first react js app</h2>
+      <form onSubmit={getdate}>
+
+
+        <input type='text' name='nm' placeholder='Enter the value'></input>
+        <br></br>
+        <br></br>
+        <input type='submit' value='add data'></input>
+        <br></br>
+        <br></br>
+        <h1>add Value is:{frame}</h1>
+      </form>
+    </>
+  );
+}
+
+export default App;
